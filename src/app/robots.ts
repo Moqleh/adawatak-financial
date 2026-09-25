ea6b588fe8edd8eeadccee03658670c8aa140f98
Moqleh <moh.oglah@gmail.com>
2026-09-25 13:13:49 +0300

    fix: make metadata routes static-export compatible

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,1 +1,2 @@
+export const dynamic='force-static';
 import type{MetadataRoute}from'next';export default function robots():MetadataRoute.Robots{return{rules:{userAgent:'*',allow:'/'},sitemap:'https://moqleh.github.io/adawatak-financial/sitemap.xml'}}
