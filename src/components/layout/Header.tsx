05aab0b38dfd0f3f5880c65dc9dc871abd1194ae
Moqleh <moh.oglah@gmail.com>
2026-09-25 11:28:59 +0300

    feat: connect primary financial navigation

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,12 +1,1 @@
-import {getTranslations} from 'next-intl/server';
-import {Link} from '@/i18n/navigation';
-import LanguageSwitcher from './LanguageSwitcher';
-
-export default async function Header(){
-  const t=await getTranslations();
-  return <header className="header"><div className="container header-inner">
-    <Link href="/" className="brand"><span className="brand-mark"><i/><i/><i/></span><span><b>{t('brand')}</b><small>Adawatak Financial</small></span></Link>
-    <nav className="nav"><Link href="/">{t('nav.home')}</Link><Link href="/tools">{t('nav.tools')}</Link><a href="#markets">{t('nav.markets')}</a><a href="#resources">{t('nav.resources')}</a><a href="#about">{t('nav.about')}</a></nav>
-    <div className="header-actions"><LanguageSwitcher/></div>
-  </div></header>
-}
+import{getTranslations}from'next-intl/server';import{Link}from'@/i18n/navigation';import LanguageSwitcher from'./LanguageSwitcher';export default async function Header(){const t=await getTranslations();return <header className="header"><div className="container header-inner"><Link href="/" className="brand"><span className="brand-mark"><i/><i/><i/></span><span><b>{t('brand')}</b><small>Adawatak Financial</small></span></Link><nav className="nav"><Link href="/">{t('nav.home')}</Link><Link href="/gold">Gold</Link><Link href="/currencies">FX</Link><Link href="/currency-converter">Converter</Link><Link href="/markets">{t('nav.markets')}</Link><Link href="/tools">{t('nav.tools')}</Link></nav><div className="header-actions"><LanguageSwitcher/></div></div></header>}
