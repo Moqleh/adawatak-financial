e405bf633c3e26996f2248ee46d505e483bf6358
Moqleh <moh.oglah@gmail.com>
2026-09-25 13:04:18 +0300

    feat: add local favorites and search metadata

---
+++ src/app/robots.ts
@@ -0,0 +1,1 @@
+import type{MetadataRoute}from'next';export default function robots():MetadataRoute.Robots{return{rules:{userAgent:'*',allow:'/'},sitemap:'https://moqleh.github.io/adawatak-financial/sitemap.xml'}}
